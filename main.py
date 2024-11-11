9f75f3fe9a2d6360c1c5dc655cc9455df1890736
Vinicius Yudi <vinizzly2005@gmail.com>
2024-11-11 20:46:40 -0300

    att cod

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import multiprocessing as mp
 import time
 
-def process(name: str, n: int, t: float, start_semaphore: mp.Semaphore, end_semaphore: mp.Semaphore = None) -> None:
+def process(name: str, n: int, t: float, start_semaphore: mp.Semaphore, end_semaphores: list = None) -> None:
     print(f'Processo {name} aguardando')
     start_semaphore.acquire()
     print(f'Processo {name} iniciado')
@@ -9,28 +9,33 @@ def process(name: str, n: int, t: float, start_semaphore: mp.Semaphore, end_sema
         print(f'{name} - {i}')
         time.sleep(t)
     print(f'Processo {name} finalizado')
-    if end_semaphore:
-        end_semaphore.release()
+    
+    if end_semaphores:
+        for sem in end_semaphores:
+            sem.release()
 
 def main() -> None:
-    range_count = int(input('Digite um valor inteiro para contagem: '))
-    time_count = float(input('Digite um valor para o tempo de contagem (em segundos): '))
+    range_count = int(input('Digite um valor para contagem: '))
+    time_count = float(input('Digite um valor para a contagem: '))
 
-    start_A = mp.Semaphore(1)  
-    start_B = mp.Semaphore(0)  
-    start_C = mp.Semaphore(0)  
-    start_D = mp.Semaphore(0)  
+    start_A = mp.Semaphore(1)   # A inicia automaticamente
+    start_B = mp.Semaphore(0)    # B espera por A
+    start_C = mp.Semaphore(0)    # C também espera por A
+    start_D = mp.Semaphore(0)    # D espera por B e C
 
-    process_A = mp.Process(target=process, args=('A', range_count, time_count, start_A, start_B))
-    process_B = mp.Process(target=process, args=('B', range_count, time_count, start_B, start_C))
-    process_C = mp.Process(target=process, args=('C', range_count, time_count, start_C, start_D))
+    # Processos
+    process_A = mp.Process(target=process, args=('A', range_count, time_count, start_A, [start_B, start_C]))
+    process_B = mp.Process(target=process, args=('B', range_count, time_count, start_B, [start_D]))
+    process_C = mp.Process(target=process, args=('C', range_count, time_count, start_C, [start_D]))
     process_D = mp.Process(target=process, args=('D', range_count, time_count, start_D))
 
+    # Iniciar processos
     process_A.start()
     process_B.start()
     process_C.start()
     process_D.start()
 
+    # Aguardar processos terminarem
     process_A.join()
     process_B.join()
     process_C.join()
